a7100514337cc4f7ef3863a0b156f39a0d2e7dc5
Danielle Metz <daniellemetz11@gmail.com>
2025-02-10 21:39:43 +0000

    end of day 1

---
+++ src/week1/TodosSolution/Todos.Api/Program.cs
@@ -0,0 +1,36 @@
+
+using Marten;
+using Todos.Api.Todos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthorization();
+
+var connectionString = builder.Configuration.GetConnectionString("todos") ?? throw new Exception("can't start the api without a connection string");
+
+builder.Services.AddMarten(builder =>
+{
+    builder.Connection(connectionString);
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//above this line is configuration for the services inside our application
+var app = builder.Build();
+//below this line is configuration for how http requests and responses are handled
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapTodos();
+
+app.Run(); //kind of an infinite for loop looking for an HTTP request to handle and send a response. multithreaded so can handle multiple requests. This service eventually assigned to a computer/cloud somewhere and just keeps listening
+
+Console.WriteLine("Done running the application");
---
+++ src/week1/TodosSolution/Todos.Api/Todos/Endpoints.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Marten;
+
+namespace Todos.Api.Todos;
+
+public static class Endpoints
+{
+    //GET /todos
+    public static IEndpointRouteBuilder MapTodos(this IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("/todos", async (IDocumentSession session) =>{
+            //var fakeData = new List<TodoListItem>()
+            //{
+            //    new TodoListItem() { Id = Guid.NewGuid(), Description = "Clean Garage", Completed = false, CreatedOn = DateTimeOffset.UtcNow} 
+            //};
+
+            var response = await session.Query<TodoListItem>().ToListAsync();
+            return Results.Ok(response);
+        });
+
+        //POST /todos
+
+        //i document session is what knows how to save to database
+        builder.MapPost("/todos", (TodoListCreateItem request, IDocumentSession session) =>{
+
+            var response = new TodoListItem
+            {
+                Id = Guid.NewGuid(),
+                Description = request.Description,
+                Completed = false,
+                CreatedOn = DateTimeOffset.UtcNow
+            };
+            return Results.Ok(response);
+        });
+        return builder;
+    }
+    
+   
+}
+
+public record TodoListItem
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+    public bool Completed { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
+    public DateTimeOffset CompletedOn { get; set; }
+}
+
+
+public record TodoListCreateItem
+{
+    public string Description { get; set; } = string.Empty;
+}
